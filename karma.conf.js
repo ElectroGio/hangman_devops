50e34f00081284ae20b12a8e490db99a8c3ecf6d
Napoleon Moreno <napoleon.moreno@credibanco.com>
2021-05-14 19:50:17 -0500

    Add reporter and configurations

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -22,13 +23,18 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
-    singleRun: false,
-    restartOnFileChange: true
+    singleRun: true,
+    restartOnFileChange: true,
+    junitReporter: {
+      outputDir: "testDirectory",
+      outputFile: "reporter.xml",
+      useBrowserName: false
+    }
   });
 };
